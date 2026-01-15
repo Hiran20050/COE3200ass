3533a1114287e5b60c5d0ed7e7900dab79f263e5
Hiran20050 <s25007990@ousl.lk>
2026-01-15 20:30:19 +0530

    Update hello.py

---
+++ hello.py
@@ -0,0 +1,10 @@
+# hello.py
+# A simple script to print a greeting message
+
+def main():
+    print("Hello! I am Hiran Erandaka.")
+    print("Welcome to my  assignment repository.")
+
+if _name_ == "_main_":
+  
+    main()
